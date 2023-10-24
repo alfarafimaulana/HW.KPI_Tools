9bd5ca837f2087c9d5fe4d518662ae41c8639f03
Alfarafi Maulana <alfarafi.maulana.am@gmail.com>
2023-10-24 15:23:12 +0700

    almost

--- plot lib example.py
+++ plot lib example.py
@@ -4,7 +4,8 @@ import csv
 import pandas as pd
 
 
-df1 = pd.read_csv('Test1.csv',sep =',')
+
+df1 = pd.read_csv('Test1.csv',sep =',',skiprows=13)
 # df2 = df1['NE Name'].str.startswith('UPBNB01')
 
 df1 = df1.sort_values(by=['NE Name','Start Time'], ascending=True, na_position='first')
@@ -12,10 +13,10 @@ DPS_ASBC01 = df1[(df1['NE Name'] == 'DPS_ASBC01')]
 DPS_ASBC02 = df1[(df1['NE Name'] == 'DPS_ASBC02')]
 DPS_ASBC03 = df1[(df1['NE Name'] == 'DPS_ASBC03')]
 
+headerinfo =  "Rate of ABCF Successful Initial Registrations (%)"
+headerinfo2 =  "Number of ABCF Successful Initial Registrations (times)"
 
-dfhead= df1.head()
-print(dfhead)
-headerinfo =  "User Plane L7 parser DNS uplink packets (number)"
+print(headerinfo.count)
 
 time = DPS_ASBC01["Start Time"]
 y1 = DPS_ASBC01[headerinfo]
@@ -24,12 +25,26 @@ y3 = DPS_ASBC03[headerinfo]
 
 
 
+y12 = DPS_ASBC01[headerinfo2]
+y22 = DPS_ASBC02[headerinfo2]
+y32 = DPS_ASBC03[headerinfo2]
+
 plt.title('Traffic')
 plt.plot(time,y1, linewidth=0.5)
 plt.plot(time,y2, linewidth=0.5)
 plt.plot(time,y3, linewidth=0.5)
 plt.xlabel('Dates')
 plt.ylabel('Nominal (juta)')
+plt.xticks([5,10,15,20])
 plt.show()
+
+# plt.title('Traffic')
+# plt.plot(time,y12, linewidth=0.5)
+# plt.plot(time,y22, linewidth=0.5)
+# plt.plot(time,y32, linewidth=0.5)
+# plt.xlabel('Dates')
+# plt.ylabel('Nominal (juta)')
+# plt.show()
+
 
 
