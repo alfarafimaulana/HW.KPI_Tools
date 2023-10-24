a99306e5398352d97342c32dbc0a66fe8e5cfb2f
Alfarafi Maulana <alfarafi.maulana.am@gmail.com>
2023-10-24 14:50:01 +0700

    For IMS FNI

--- plot lib example.py
+++ plot lib example.py
@@ -4,35 +4,30 @@ import csv
 import pandas as pd
 
 
-df1 = pd.read_csv('traffic.csv',sep =',')
+df1 = pd.read_csv('Test1.csv',sep =',')
 # df2 = df1['NE Name'].str.startswith('UPBNB01')
 
 df1 = df1.sort_values(by=['NE Name','Start Time'], ascending=True, na_position='first')
-UPCBT03 = df1[(df1['NE Name'] == 'UPCBT03')]
-UPPLB02 = df1[(df1['NE Name'] == 'UPPLB02')]
-UPBNB02 = df1[(df1['NE Name'] == 'UPBNB02')]
-UPPNB04 = df1[(df1['NE Name'] == 'UPPNB04')]
-UPPNB06 = df1[(df1['NE Name'] == 'UPPNB06')] 
+DPS_ASBC01 = df1[(df1['NE Name'] == 'DPS_ASBC01')]
+DPS_ASBC02 = df1[(df1['NE Name'] == 'DPS_ASBC02')]
+DPS_ASBC03 = df1[(df1['NE Name'] == 'DPS_ASBC03')]
+
 
 dfhead= df1.head()
 print(dfhead)
-
 headerinfo =  "User Plane L7 parser DNS uplink packets (number)"
 
-time = UPCBT03["Start Time"]
-y = UPCBT03[headerinfo]
-y1 = UPPLB02[headerinfo]
-y2 = UPBNB02[headerinfo]
-y3 = UPPNB04[headerinfo]
-y4 = UPPNB06[headerinfo]
+time = DPS_ASBC01["Start Time"]
+y1 = DPS_ASBC01[headerinfo]
+y2 = DPS_ASBC02[headerinfo]
+y3 = DPS_ASBC03[headerinfo]
+
 
 
 plt.title('Traffic')
-plt.plot(time,y, linewidth=0.5)
 plt.plot(time,y1, linewidth=0.5)
 plt.plot(time,y2, linewidth=0.5)
 plt.plot(time,y3, linewidth=0.5)
-plt.plot(time,y4, linewidth=0.5)
 plt.xlabel('Dates')
 plt.ylabel('Nominal (juta)')
 plt.show()
