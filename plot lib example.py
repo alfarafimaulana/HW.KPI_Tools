9954903f27548986acd8999eeab7e136d977b6fa
Alfarafi Maulana <alfarafi.maulana.am@gmail.com>
2023-10-26 17:36:43 +0700

    Improved version but not finished

--- plot lib example.py
+++ plot lib example.py
@@ -9,14 +9,14 @@ df1 = pd.read_csv('Test1.csv',sep =',',skiprows=13)
 # df2 = df1['NE Name'].str.startswith('UPBNB01')
 
 df1 = df1.sort_values(by=['NE Name','Start Time'], ascending=True, na_position='first')
-DPS_ASBC01 = df1[(df1['NE Name'] == 'DPS_ASBC01')]
-DPS_ASBC02 = df1[(df1['NE Name'] == 'DPS_ASBC02')]
-DPS_ASBC03 = df1[(df1['NE Name'] == 'DPS_ASBC03')]
+DPS_ASBC01 = df1[(df1['NE Name'] == 'SNBASBC01')]
+DPS_ASBC02 = df1[(df1['NE Name'] == 'SNBASBC02')]
+DPS_ASBC03 = df1[(df1['NE Name'] == 'SNBASBC03')]
 
 headerinfo =  "Rate of ABCF Successful Initial Registrations (%)"
 headerinfo2 =  "Number of ABCF Successful Initial Registrations (times)"
 
-xdesc = (df1['Period (min)'].value_counts()[5])
+xdesc = (df1['NE Name'].value_counts()['SNBASBC01'])
 
 time = DPS_ASBC01["Start Time"]
 y1 = DPS_ASBC01[headerinfo]
@@ -27,6 +27,7 @@ y12 = DPS_ASBC01[headerinfo2]
 y22 = DPS_ASBC02[headerinfo2]
 y32 = DPS_ASBC03[headerinfo2]
 
+print(xdesc)
 # headerinfocount = time.count
 # print(headerinfocount)
 
@@ -36,7 +37,7 @@ plt.plot(time,y2, linewidth=0.5)
 plt.plot(time,y3, linewidth=0.5)
 plt.xlabel('Dates')
 plt.ylabel('Nominal (juta)')
-plt.xticks([0, xdesc/9])
+plt.xticks([0, xdesc/2,xdesc-1])
 plt.show()
 
 # plt.title('Traffic')
@@ -46,6 +47,8 @@ plt.show()
 # plt.xlabel('Dates')
 # plt.ylabel('Nominal (juta)')
 # plt.show()
+
+
 
 
 
